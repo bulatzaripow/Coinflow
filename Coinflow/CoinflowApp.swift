ecdbcfed776155302adf3a5467a95755e1c5858c
Bulat Zaripov <zaripov291096@gmail.com>
2025-12-17 19:22:29 +0300

    Initial Commit

---
+++ Coinflow/CoinflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinflowApp.swift
+//  Coinflow
+//
+//  Created by Bulat Zaripov on 17.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
