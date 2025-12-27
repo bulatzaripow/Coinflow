cb3a8cfb9ffecb7f014627621e89eef14df6a8b4
Bulat Zaripov <zaripov291096@gmail.com>
2025-12-27 22:40:41 +0300

    refactor(COIN-5): remove unnecessary ZStack

--- Coinflow/Core/Modules/Home/Views/HomeView.swift
+++ Coinflow/Core/Modules/Home/Views/HomeView.swift
@@ -27,101 +27,99 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
+            ZStack(alignment: .bottom) {
                 Color(.appBackground)
                     .ignoresSafeArea()
                 
-                ZStack(alignment: .bottom) {
-                    ScrollView {
-                        VStack(spacing: 20) {
-                            // Header
-                            HStack(spacing: 0) {
-                                VStack(alignment: .leading, spacing: 3) {
-                                    Text(DateFormatterHelper.formatDayMonth(Date()))
-                                        .font(.caption)
-                                        .foregroundStyle(.secondary)
-                                    
-                                    Text("Good Morning, Bulat")
-                                        .fontWeight(.bold)
-                                }
+                ScrollView {
+                    VStack(spacing: 20) {
+                        // Header
+                        HStack(spacing: 0) {
+                            VStack(alignment: .leading, spacing: 3) {
+                                Text(DateFormatterHelper.formatDayMonth(Date()))
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
                                 
-                                Spacer()
-                                
-                                NavigationLink {
-                                    
-                                } label: {
-                                    Image("user-beard")
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 38, height: 38)
-                                        .background(.gray.opacity(0.2))
-                                        .clipShape(Circle())
-                                }
+                                Text("Good Morning, Bulat")
+                                    .fontWeight(.bold)
                             }
-                            .padding(.top, 10)
-                            .padding(.horizontal, 20)
                             
-                            // Accounts
-                            AccountCarouselTabView(accounts: accounts)
+                            Spacer()
                             
-                            // Transactions
-                            TransactionsListView(transactions: transactions)
-                                .padding(.bottom, 70)
+                            NavigationLink {
+                                
+                            } label: {
+                                Image("user-beard")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 38, height: 38)
+                                    .background(.gray.opacity(0.2))
+                                    .clipShape(Circle())
+                            }
                         }
+                        .padding(.top, 10)
+                        .padding(.horizontal, 20)
+                        
+                        // Accounts
+                        AccountCarouselTabView(accounts: accounts)
                     }
                     
-                    // Bottom actions
-                    HStack(spacing: 18) {
-                        Button(action: {
-                            // TODO: open accounts / wallet
-                        }) {
-                            Image("chart-simple")
-                                .resizable()
-                                .scaledToFit()
-                                .padding(13)
-                                .font(.system(size: 20, weight: .semibold))
-                                .frame(width: 45, height: 45)
-                                .background(.white)
-                                .clipShape(Circle())
-                                .shadow(
-                                    color: Color.black.opacity(0.06),
-                                    radius: 14,
-                                    x: 0,
-                                    y: 6
-                                )
-                        }
-                        
-                        Button(action: {
-                            // TODO: add expense
-                        }) {
-                            HStack(spacing: 5) {
-                                Image(systemName: "plus")
-                                    .font(.headline)
-                                
-                                Text("Add")
-                                    .font(.headline)
-                            }
-                            .foregroundStyle(.white)
-                            .frame(maxWidth: .infinity, maxHeight: 45)
-                            .background(Color(.appPrimary))
-                            .clipShape(Capsule())
-                            .shadow(
-                                color: Color.black.opacity(0.08),
-                                radius: 20,
-                                x: 0,
-                                y: 10
-                            )
-                        }
-                    }
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 5)
-                    .shadow(
-                        color: Color.black.opacity(0.08),
-                        radius: 20,
-                        x: 0,
-                        y: 10
-                    )
+                    // Transactions
+                    TransactionsListView(transactions: transactions)
+                        .padding(.bottom, 70)
                 }
+                
+                // Bottom actions
+                HStack(spacing: 18) {
+                    Button(action: {
+                        // TODO: open accounts / wallet
+                    }) {
+                        Image("chart-simple")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(13)
+                            .font(.system(size: 20, weight: .semibold))
+                            .frame(width: 45, height: 45)
+                            .background(.white)
+                            .clipShape(Circle())
+                            .shadow(
+                                color: Color.black.opacity(0.06),
+                                radius: 14,
+                                x: 0,
+                                y: 6
+                            )
+                    }
+                    
+                    Button(action: {
+                        // TODO: add expense
+                    }) {
+                        HStack(spacing: 5) {
+                            Image(systemName: "plus")
+                                .font(.headline)
+                            
+                            Text("Add")
+                                .font(.headline)
+                        }
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity, maxHeight: 45)
+                        .background(Color(.appPrimary))
+                        .clipShape(Capsule())
+                        .shadow(
+                            color: Color.black.opacity(0.08),
+                            radius: 20,
+                            x: 0,
+                            y: 10
+                        )
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+                .shadow(
+                    color: Color.black.opacity(0.08),
+                    radius: 20,
+                    x: 0,
+                    y: 10
+                )
             }
         }
     }
